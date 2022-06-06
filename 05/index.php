7007636e00c7ae5a756672768a8dd6bdce42827a
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-06 19:15:39 +0300

    Update index.php

--- 05/index.php
+++ 05/index.php
@@ -2,10 +2,13 @@
 
 // WARNING. CONST not change!
 
+// First version declaration const
 define("CONST_1", "value 1");
 
 echo CONST_1;
 
+// Second version declaration const
+// Only top page
 const CONST_2 = 'value 2';
 
 var_dump(CONST_2);
