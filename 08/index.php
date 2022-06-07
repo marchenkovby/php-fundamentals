3ebd94c9da7f3f67e5e13379c92c62fab98e7dd6
Maksim <marchenkovby@gmail.com>
2022-06-07 17:54:29 +0300

    Finish lesson 8

---
+++ 08/index.php
@@ -0,0 +1,31 @@
+<?php error_reporting(-1);
+
+// Operators
+// Arithmetic operators
+//$a = '10';
+//$a = -$a;
+//var_dump($a);
+//var_dump(+'-10.3');
+
+// 1 - left operand
+// 2 - right operand
+// + - arithmetic operator
+//var_dump(1 + 2);
+//var_dump(1 - 2);
+//var_dump(10 * 2);
+//var_dump(10 * 2);
+//var_dump(10 / 2);
+
+//var_dump(1 + '2t');
+//var_dump(1 + 't2');
+
+//var_dump("str10" + 20); // Function not exec (critical error)
+
+var_dump(200 % 7);
+var_dump(-200 % 7); //-4
+var_dump(200 % -7); //4
+
+var_dump(2**4);
+
+
+
