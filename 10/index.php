af966281ad4813d352b1e1734ac602a0191ca701
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-15 09:45:59 +0300

    Update lesson 10

--- 10/index.php
+++ 10/index.php
@@ -4,5 +4,9 @@
 //var_dump(3 === 3); // true
 
 // != and <>
-var_dump(2 != 3); // true
-var_dump(2 <> 3); // true
+//var_dump(2 != 3); // true
+//var_dump(2 <> 3); // true
+//var_dump('3' !== '3'); // false
+//var_dump(3 !== '3'); // true
+
+var_dump(2 > 3); // false
