0c7a1036d323fcf767f72fb0a3d1c783bd893e43
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-17 09:37:11 +0300

    Update lesson 15

--- 15/index.php
+++ 15/index.php
@@ -2,11 +2,27 @@
 
 // Control structures (Управляющие конструкции)
 
-// Loop While
+// Loop: While
 
 // CRTL + SHIFT + ENTER
-$i = 1;
-while ($i <= 1000) {
-    echo "{$i}<br>";
-    $i = $i + 1; // $i++ or $i += 1;
+// $i = 1;
+// while ($i <= 1000) {
+//     echo "{$i}<br>";
+//     $i = $i + 1; // $i++ or $i += 1;
+// }
+
+echo "<table border=\"1\" wdth=\"100%\">";
+
+$tr = 1;
+while ($tr <= 10) {
+    echo "<tr>";
+        $td = 1;
+        while ($td <= 20) {
+            echo "<td>{$td}</td>";
+            $td++;
+        }
+    echo "</tr>";
+    $tr++;
 }
+
+echo "</table>"
