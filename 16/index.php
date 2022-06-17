e8be90abaa0ade2275113c71585adca0f9229982
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-17 11:35:54 +0300

    Update lesson 16

--- 16/index.php
+++ 16/index.php
@@ -1,5 +1,7 @@
 <?php error_reporting(-1);
 
+
+// Task 1
 echo "<select>";
 $year = 1990;
 while ($year <= 2021) {
@@ -7,3 +9,20 @@ while ($year <= 2021) {
     $year++;
 }
 echo "<select>";
+
+// Task 2
+echo "<table border=\"1\" width=\"100%\">";
+
+$x = 1;
+while ($x < 10) {
+    echo "<tr>";
+    $y = 1;
+    while ($y < 10) {
+        echo "<td>" . $x * &y . "</td>";
+        $y++;
+    }
+    echo "</tr>";
+    $x++''
+}
+
+echo "</table>";
