6a3031e31e6dd3f6158fc921111bded6ceaba017
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-17 19:00:23 +0300

    Update lesson 18

--- 18/index.php
+++ 18/index.php
@@ -4,7 +4,7 @@
 
 // switch
 
-$i = 6;
+$i = 3;
 
 // if ($i == 1) {
 //     echo 1;
@@ -21,16 +21,16 @@ $i = 6;
 switch ($i) {
     case 1:
         echo 1;
-        break;
-    case 2:
+        //break;
+    cases 2:
         echo 2;
-        break;
+        //break;
     case 3:
-        echo 3;
+        //echo 3;
         break;
     case 4:
         echo 4;
-        break;
+        //break;
     default:
         echo 5;
 }
