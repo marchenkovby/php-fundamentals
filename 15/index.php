ebbbe79a2ad8a7a0c3fcff2a884a17c387cc79a0
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-17 10:04:43 +0300

    Update lesson 15

--- 15/index.php
+++ 15/index.php
@@ -11,18 +11,30 @@
 //     $i = $i + 1; // $i++ or $i += 1;
 // }
 
-echo "<table border=\"1\" width=\"100%\">";
+// echo "<table border=\"1\" width=\"100%\">";
 
-$tr = 1;
-while ($tr <= 10) {
-    echo "<tr>";
-        $td = 1;
-        while ($td <= 10) {
-            echo "<td>{$tr}</td>";
-            $td++;
-        }
-    echo "</tr>";
-    $tr++;
-}
+// $tr = 1;
+// while ($tr <= 10) {
+//     echo "<tr>";
+//         $td = 1;
+//         while ($td <= 10) {
+//             echo "<td>{$tr}</td>";
+//             $td++;
+//         }
+//     echo "</tr>";
+//     $tr++;
+// }
 
-echo "</table>";
+// echo "</table>";
+
+// $i = 1;
+// while ($i <= 1000) {
+//     echo "{$i}<br>";
+//     $i = $i + 1; // $i++ or $i += 1;
+// }
+
+$i = 11;
+do {
+    echo "{$i}<br>";
+    $i++;
+} while ($i <= 10);
