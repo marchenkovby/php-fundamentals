127110dedd5099f82ad411999231b4e0327f51cc
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-17 18:37:16 +0300

    Update lesson 18

--- 18/index.php
+++ 18/index.php
@@ -2,4 +2,18 @@
 
 // Control structures (Управляющие конструкции)
 
-//
+// switch
+
+$i = 3;
+
+if ($i == 1) {
+    echo 1;
+} elseif ($i == 2) {
+    echo 2;
+} elseif ($i == 3) {
+    echo 3;
+} else ($i == 4) {
+    echo 4;
+} else {
+    echo 5;
+}
