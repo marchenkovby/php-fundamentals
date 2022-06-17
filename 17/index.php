e06628e45a4ac975f110bff62cee52879641544d
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-17 15:14:25 +0300

    Update lesson 17

--- 17/index.php
+++ 17/index.php
@@ -10,6 +10,20 @@
 
 // for ($i = 1, $j = 0; $i <= 10; $j += $i, print $i, $i++);
 
-for ($i = 20; $i >= 1; $i--) {
-    echo "{$i}<br>";
+// for ($i = 20; $i >= 1; $i--) {
+//     echo "{$i}<br>";
+// }
+
+// echo "<select>";
+// $year = 1900;
+// while ($year <= 2021) {
+//     echo "<option>{$year}</option>";
+//     $year++;
+// }
+// echo "<select>";
+
+echo "<select>";
+for($year = 1900; $year<= 2021; $year++)
+    echo "<option>{$year}</option>";
 }
+echo "<select>";
