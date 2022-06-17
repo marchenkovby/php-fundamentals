a6622db7e3ff0bf8cfe0ea810899b922863b8010
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-17 11:54:07 +0300

    Update lesson 16

--- 16/index.php
+++ 16/index.php
@@ -18,7 +18,7 @@ while ($x < 10) {
     echo "<tr>";
     $y = 1;
     while ($y < 10) {
-        echo "<td>{$x}*{$y} = " . $x*$y . "</td>";
+        echo "<td>{$y}*{$x} = " . $y*$x . "</td>";
         $y++;
     }
     echo "</tr>";
