6b6ddb8d91654f5b213d89110ec7779e4d7ab976
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-17 08:51:18 +0300

    Update lesson 14

--- 14/index.php
+++ 14/index.php
@@ -1,1 +1,30 @@
 <?php error_reporting(-1);
+
+// Types
+
+// NULL
+// NULL if:
+// - $a = null;
+// - $a
+// - unset()
+$price = 10;
+//var_dump($price);
+
+
+// Operators
+
+// Ternary operator "?:"
+//$light = 'green';
+
+//if ($light == 'green') {
+//    echo 'You can go now';
+//} else {
+//    echo 'You can\'t go now';
+//}
+
+//echo ($light == 'green') ? 'You can go now' : 'You can\'t go now';
+
+//echo $price ? $price : 'unknown price';
+
+//echo $price ?: 'unknown price';
+echo $price >= 10 ?: 'unknown price';
