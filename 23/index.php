89f96e91ade7aa8b087fe3bdca3b84440485e2dd
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-20 08:42:07 +0300

    Update lesson 23

--- 23/index.php
+++ 23/index.php
@@ -4,9 +4,15 @@
 
 $nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+// for ($i = 0; $i <= 10; $i++) {
+//     if ($nums[$i] % 2) {
+//         continue;
+//     }
+//     echo $nums[$i] . "<br>";
+// }
+
 for ($i = 0; $i <= 10; $i++) {
-    if ($nums[$i] % 2) {
-        continue;
+    if (!($nums[$i] % 2)) {
+        echo $nums[$i] . "<br>";
     }
-    echo $nums[$i] . "<br>";
 }
