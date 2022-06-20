35d524cc2aff831ccd8752ab699f204dde80d0ed
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-20 08:47:21 +0300

    Update lesson 23

--- 23/index.php
+++ 23/index.php
@@ -11,8 +11,33 @@ $nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //     echo $nums[$i] . "<br>";
 // }
 
-for ($i = 0; $i <= 10; $i++) {
-    if (!($nums[$i] % 2)) {
-        echo $nums[$i] . "<br>";
-    }
+// for ($i = 0; $i <= 10; $i++) {
+//     if (!($nums[$i] % 2)) {
+//         echo $nums[$i] . "<br>";
+//     }
+// }
+
+$goods = [
+    [
+        'title' => 'Nokia',
+        'price' => 100,
+        'qty' => 10
+    ],
+    [
+        'title' => 'Sony',
+        'price' => 120,
+        'qty' => 7
+    ],
+    [
+        'title' => 'LG',
+        'price' => 105,
+        'qty' => 15
+    ]
+];
+
+for ($i = 0; $i <= 2; $i++) {
+   echo "Title: {$good[$i]['title']} <br>";
+   echo "Price: {$good[$i]['price']} <br>";
+   echo "Qty: {$good[$i]['qty']} <br>";
+   echo '<hr>';
 }
