22eb4eef5d04f83b8fc783226650a4a5106f8f2a
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-21 08:37:43 +0300

    Update lesson 26

--- 26/index.php
+++ 26/index.php
@@ -1,3 +1,15 @@
 <?php error_reporting(-1);
 
 // User-defined functions
+
+
+$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
+
+function get_count(...$arr)
+{
+    $qty = 0;
+    foreach ($arr as $k => $v) {
+        $qty++
+    }
+    return $qty;
+}
