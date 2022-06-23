940dfd80cefaf2fe95da2e6fd65bcd2c29e27ee7
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-23 19:22:21 +0300

    Update lesson 26

--- 26/index.php
+++ 26/index.php
@@ -2,16 +2,32 @@
 
 // User-defined functions
 
-
 $arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,];
+
+$goods = [
+    [
+        'title' => 'Nokia',
+        'price' => 100,
+        'qty' => 10
+    ],
+    [
+        'title' => 'Sony',
+        'price' => 120,
+        'qty' => 7
+    ],
+    [
+        'title' => 'LG',
+        'price' => 105,
+        'qty' => 15
+    ]
+];
 
 function get_count(...$arr)
 {
     $qty = 0;
     foreach ($arr as $k => $v) {
-        $qty++;
+        echo $v . '<br>';
     }
-    return $qty;
 }
 
 echo get_count($arr);
