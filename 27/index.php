68c7281047005559a21d779d0ae7be749df2ac01
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-25 09:19:28 +0300

    Update lesson 27

--- 27/index.php
+++ 27/index.php
@@ -1,15 +1,4 @@
 <?php error_reporting(-1);
 
-$host="192.168.0.104";
-
-exec("ping -c 4 " . $host, $output, $result);
-
-print_r($output);
-
-if ($result == 0)
-
-echo "Ping successful!";
-
-else
-
-echo "Ping unsuccessful!";
+$result = exec("/bin/ping -c 1 -W 1 google.by"); 
+echo "result=".$result."\n";
