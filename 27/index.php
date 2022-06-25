7f65288f2f767f8540aa004e9f524e0cda103637
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-25 09:15:46 +0300

    Update lesson 27

--- 27/index.php
+++ 27/index.php
@@ -1,2 +1,13 @@
 <?php error_reporting(-1);
 
+function pingAddress($ip) {
+    $pingresult = exec("/bin/ping -n 3 $ip", $outcome, $status);
+    if (0 == $status) {
+        $status = "alive";
+    } else {
+        $status = "dead";
+    }
+    echo "The IP address, $ip, is  ".$status;
+}
+
+pingAddress("127.0.0.1");
