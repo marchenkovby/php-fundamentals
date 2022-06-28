eeacf77c994cb1f7f6cf3d530440ed054553e3f6
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-28 10:50:47 +0300

    Begin lesson 28

--- 28/index.php
+++ 28/index.php
@@ -1,2 +1,26 @@
-<?php error_reporting(-1);
+<?php error_reporting(-1); ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+<body>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptas culpa minima, quidem ipsum reiciendis, ratione perferendis repellat autem sunt ad, a enim error labore nulla voluptate magnam eaque quos!</p>
+
+<?php
+
+$nums = [1, 2, 3, 4, 5]
+
+foreach ($nums as $num) {
+    echo "<p>$num</p>";
+}
+
+?>
+
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptas culpa minima, quidem ipsum reiciendis, ratione perferendis repellat autem sunt ad, a enim error labore nulla voluptate magnam eaque quos!</p>
+</body>
+</html>
