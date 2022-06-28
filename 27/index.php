07a6a4a21139d9317f4efad08d428030103840e4
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-28 08:22:06 +0300

    Update lesson 27

--- 27/index.php
+++ 27/index.php
@@ -1,16 +1,5 @@
 <?php error_reporting(-1);
 
-function get_table($tr, $td) 
-{
-    $table = '<table border=\"1\" width=\"100%\">';
-    for ($x = 1; $x <= $tr; $x++) {
-        $table .= "<tr>";
-        for ($y = 1; $y <= $td; $y++) {
-            $table .= "<td>{$y}*{$x} = " . $y * $x . "</td>";
-        }
-        $table .= "</tr>";
-    }
-    return $table;
-}
+
 
 echo get_table(5, 7);
