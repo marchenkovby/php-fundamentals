9d43e28663d722fbbac0039c91027ca3f11d928b
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-28 09:49:54 +0300

    Update lesson 27

--- 27/conf.php
+++ 27/conf.php
@@ -1,6 +1,6 @@
 <?php
 
-$data = [
+return [
     'param1' => 'Value1',
     'param2' => 'Value2',
     'param3' => 'Value3'
--- 27/index.php
+++ 27/index.php
@@ -13,7 +13,7 @@
 // require 'funcs.php'; 
 // echo get_table(5, 7);
 
-require_once 'conf.php';
+$data = require_once 'conf.php';
 
 /** $var array $date */
 print_r($data);
