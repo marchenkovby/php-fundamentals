93b2192de35ca28325ce544a7c3eddfbce1453c6
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-29 17:59:25 +0300

    Begin lesson 30

---
+++ 30/index.php
@@ -0,0 +1,19 @@
+<?php error_reporting(-1);
+
+function debug($data)
+{
+    echo '<pre>' . print_r($data, 1) . '</pre>';
+}
+
+$nums = [1, 2, 3, 1, 4, 5, 3, 2, 6, 7, 7, 8, 8, 9, 2, 5,];
+
+function user_count(array $arr): int
+{   
+    $qrt = 0;
+    foreach ($arr as $value) {
+        $qty++;
+    }
+    return $qty;
+}
+
+echo user_count($nums);
