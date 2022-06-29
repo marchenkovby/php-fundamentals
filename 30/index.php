7616df22e5c1f725b68642b58cba1ce6e8657067
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-29 18:02:03 +0300

    Update lesson 30

--- 30/index.php
+++ 30/index.php
@@ -7,6 +7,7 @@ function debug($data)
 
 $nums = [1, 2, 3, 1, 4, 5, 3, 2, 6, 7, 7, 8, 8, 9, 2, 5,];
 
+// 1
 function user_count(array $arr): int
 {   
     $qrt = 0;
@@ -17,3 +18,15 @@ function user_count(array $arr): int
 }
 
 echo user_count($nums);
+
+
+// 2
+function sum_array ($array $arr)
+{   $qty = 0
+    foreach ($arr as $value) {
+        $qty += $value;
+    }
+    return $qty;
+}
+
+echo sum_array($qty);
