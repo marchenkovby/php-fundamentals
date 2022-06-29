6468b126d6a257bcde789b0f55ac3735b6cc0b70
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-29 10:24:26 +0300

    Update lesson 29

--- 29/index.php
+++ 29/index.php
@@ -1,1 +1,36 @@
 <?php error_reporting(-1);
+
+function debug($data)
+{
+    echo '<pre>' . print_r($data, 1) . '</pre>';
+}
+
+$goods = [
+    [
+        'title' => 'Nokia',
+        'price' => 100,
+        'qty' => 10
+    ],
+    [
+        'title' => 'Sony',
+        'price' => 120,
+        'qty' => 7
+    ],
+    [
+        'title' => 'LG',
+        'price' => 105,
+        'qty' => 15
+    ]
+];
+
+$car = [
+    'brand' => 'Toyota',
+    'model' => 'Camry',
+    'year' => 2021,
+    'speed' => 220,
+    'wheels' => 4,
+];
+
+$nums = [1, 2, 3, 1, 4, 5, 3, 2, 6, 7, 7, 8, 8, 9, 2, 5,];
+
+echo count($goods);
