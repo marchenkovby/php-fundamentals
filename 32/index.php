9fa3b3d45bce8f6b35935887f8d904a956f8cfd4
Maksim <marchenkovby@gmail.com>
2022-07-09 15:18:07 +0300

    Begin lesson 32

---
+++ 32/index.php
@@ -0,0 +1,12 @@
+<?php error_reporting(-1);
+
+function debug($data): void
+{
+    echo '<pre>' . print_r($data, 1) . '</pre>';
+}
+
+$str = ' Hello, world! ';
+$str2 = 'Привет, мир!';
+$str3 = "<script>alert('©Hello &gt;' + \" world\")</script>";
+$str4 = "Привет!\nВ первых строках своего пистма...";
+
