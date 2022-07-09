371b08e1f1b587c51b57503e9e064aef136b63a4
Maksim <marchenkovby@gmail.com>
2022-07-09 15:16:22 +0300

    Update lesson 31

--- 31/index.php
+++ 31/index.php
@@ -7,7 +7,27 @@ function debug($data): void
 
 $str = ' Hello, world! ';
 $str2 = 'Привет, мир!';
-$str3 = "<script>alert('@Hello &gt;' + \" world\")</script>";
+$str3 = "<script>alert('©Hello &gt;' + \" world\")</script>";
 $str4 = "Привет!\nВ первых строках своего пистма...";
 
-echo strlen($str2);
+// echo strlen($str);
+// echo mb_strlen($str2, 'UTF-8');
+
+//$data = explode(', ', $str2);
+//debug($data);
+
+//echo implode(', ', $data);
+
+// echo htmlspecialchars($str3, ENT_QUOTES, double_encode: false);
+// echo htmlentities($str3, double_encode: false);
+
+//echo ltrim($str);
+//echo rtrim($str);
+//echo trim($str, '');
+
+//echo nl2br($str4, false);
+
+//debug(str_split($str, 3));
+
+//var_dump(substr($str, -7, 5));
+//var_dump(substr($str, -7, 5));
