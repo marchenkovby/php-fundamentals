f69bd010df37256995367f06583c85400e94cd46
Maksim Marchenkov <marchenkovby@gmail.com>
2022-07-19 18:36:36 +0300

    Начал урок 5

---
+++ 2/5/index.php
@@ -0,0 +1,17 @@
+<?php
+
+error_reporting(-1);
+
+require_once 'classes/Car.php';
+
+function debug(object $data): void
+{
+    echo '<pre>' . print_r($data, true) . '</pre>';
+}
+
+$car1 = new Car('black', 4, 180, 'volvo');
+//$car2 = new Car('black', 4, 180, 'bmv');
+
+echo $car1->getCarInfo();
+$car1->getLorem();
+//echo $car2->getCarInfo();
